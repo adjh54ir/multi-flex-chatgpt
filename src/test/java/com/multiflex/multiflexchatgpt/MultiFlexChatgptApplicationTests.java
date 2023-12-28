189d933370cea5735ff2c241bc240ef2e88ae3fe
이종훈 <adjh54ir@gmail.com>
2023-12-28 17:57:55 +0900

    Init Commit

---
+++ src/test/java/com/multiflex/multiflexchatgpt/MultiFlexChatgptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiflex.multiflexchatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiFlexChatgptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
