a684d5fa6d7330ac3dde923e6f24bdcfe448f2e7
이종훈 <adjh54ir@gmail.com>
2023-12-28 17:57:55 +0900

    Init Commit

---
+++ src/main/java/com/multiflex/multiflexchatgpt/MultiFlexChatgptApplication.java
@@ -0,0 +1,13 @@
+package com.multiflex.multiflexchatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiFlexChatgptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiFlexChatgptApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/multiflex/multiflexchatgpt/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.multiflex.multiflexchatgpt;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MultiFlexChatgptApplication.class);
+    }
+
+}
