37a1995e5e80bbe8ce96ca8be70275c0d48c752c
jonghoon_lee <adjh54ir@gmail.com>
2023-12-30 14:25:24 +0900

    prompt 구성 완료

--- src/main/java/com/multiflex/multiflexchatgpt/config/ChatGPTConfig.java
+++ src/main/java/com/multiflex/multiflexchatgpt/config/ChatGPTConfig.java
@@ -3,6 +3,8 @@ package com.multiflex.multiflexchatgpt.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -13,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
  * @since : 12/29/23
  */
 @Configuration
-public class ApiRestTemplate {
+public class ChatGPTConfig {
 
 
     @Value("${openai.secret-key}")
@@ -24,10 +26,20 @@ public class ApiRestTemplate {
 
         RestTemplate restTemplate = new RestTemplate();
 
-        restTemplate.getInterceptors().add((request, body, execution) -> {
-            request.getHeaders().add("Authorization", "Bearer " + secretKey);
-            return execution.execute(request, body);
-        });
+//        restTemplate.getInterceptors().add((request, body, execution) -> {
+//            request.getHeaders().add("Authorization", "Bearer " + secretKey);
+//            return execution.execute(request, body);
+//        });
         return restTemplate;
+    }
+
+
+    @Bean
+    public HttpHeaders httpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + secretKey);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+
     }
 }
